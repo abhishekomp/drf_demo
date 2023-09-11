ed4b3d7792d3a31d70d401b2491134afad43cb5a
abhishekomp <abhishek2283@hotmail.com>
2023-09-11 14:26:45 +0200

    Explicit Serializer usage

--- posts/urls.py
+++ posts/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("homepage/", views.homepage, name="posts_home"),
-    path("", views.api_endpoints, name="api_endpoints"),
+    #path("homepage/", views.homepage, name="posts_home"),
+    path("/post-api/", views.api_endpoints, name="api_endpoints"),
     #path("<int:post_index>", views.post_detail, name="post_detail"),
 ]
