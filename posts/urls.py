26510f810f3bda7b0f5e66f18f638bfdabc492fe
abhishekomp <abhishek2283@hotmail.com>
2023-09-13 18:22:36 +0200

    Implemented class based views to segregate get,post from put, getSingle, delete

--- posts/urls.py
+++ posts/urls.py
@@ -1,12 +1,22 @@
 from . import views
 from django.urls import path
 
+# urlpatterns for the function based view implementation
+# urlpatterns = [
+#     #path("homepage/", views.homepage, name="posts_home"),
+#     path("", views.api_endpoints, name="api_endpoints"),
+#     path("post-list/", views.list_posts, name="list_posts"),
+#     path("post-detail/<str:pk>", views.post_detail, name="post-detail"),
+#     path("post-create/", views.post_create, name="post-create"),
+#     path("post-update/<str:pk>", views.post_update, name="post-update"),
+#     path("post-delete/<str:pk>", views.post_delete, name="post-delete"),
+# ]
+
+# urlpatterns for the class based view implementation
 urlpatterns = [
-    #path("homepage/", views.homepage, name="posts_home"),
-    path("", views.api_endpoints, name="api_endpoints"),
-    path("post-list/", views.list_posts, name="list_posts"),
-    path("post-detail/<str:pk>", views.post_detail, name="post-detail"),
-    path("post-create/", views.post_create, name="post-create"),
-    path("post-update/<str:pk>", views.post_update, name="post-update"),
-    path("post-delete/<str:pk>", views.post_delete, name="post-delete"),
+    path(
+        "", views.api_endpoints, name="api_endpoints"
+    ),  # this maps to a function based view to return all the available endpoints
+    path("postAPIView", views.PostsAPIView.as_view()),
+    path("postDetailAPIView/<int:pk>", views.PostDetailAPIView.as_view()),
 ]
--- posts/views.py
+++ posts/views.py
@@ -7,24 +7,78 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 
 from .models import Post
+
+# class based view
+from rest_framework.views import APIView
+from django.http import Http404
+
 # Create your views here.
 
-def homepage(request:HttpRequest):
-  #response = {"message": "Hello Django Rest Framework"}
-  return JsonResponse("Hello Django Rest Framework", safe=False)
 
-@api_view(['GET'])
-def api_endpoints(request:HttpRequest):
-  api_urls = {
-    'list_posts': '/post-list/',
-    'post_detail': '/post-detail/<str:pk>/',
-    'post-create': '/post-create/',
-    'post-update': '/post-update/<str:pk>/',
-    'post-delete': '/post-delete/<str:pk>/',
-  }
-  response = {"api_endpoints": api_urls}
-  #return JsonResponse(data=response)
-  return Response(api_urls)   # this is the response from the rest framework for a better view in the browser
+# Class based view
+class PostsAPIView(APIView):
+    def get(self, request):
+        posts = Post.objects.all()  # get the querySet
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class PostDetailAPIView(APIView):
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(id=pk)  # get the specific post
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk, format=None):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        # return Response({"message": "Item Deleted"}, status=status.HTTP_204_NO_CONTENT)
+        return Response({"message": "Item Deleted"}, status=status.HTTP_204_NO_CONTENT)
+
+
+# Function based view
+def homepage(request: HttpRequest):
+    # response = {"message": "Hello Django Rest Framework"}
+    return JsonResponse("Hello Django Rest Framework", safe=False)
+
+
+# This method returns the available endpoints for the api
+# Function based view
+@api_view(["GET"])
+def api_endpoints(request: HttpRequest):
+    api_urls = {
+        "list_posts": "/post-list/",
+        "post_detail": "/post-detail/<str:pk>/",
+        "post-create": "/post-create/",
+        "post-update": "/post-update/<str:pk>/",
+        "post-delete": "/post-delete/<str:pk>/",
+    }
+    response = {"api_endpoints": api_urls}
+    # return JsonResponse(data=response)
+    return Response(
+        api_urls
+    )  # this is the response from the rest framework for a better view in the browser
+
 
 # Using JsonResponse to return the serialized data. When using JsonResponse, then no need to use @api_view
 # def list_posts(request):
@@ -33,44 +87,40 @@ def api_endpoints(request:HttpRequest):
 #       serializer = PostSerializer(posts, many=True)
 #       return JsonResponse(serializer.data, safe=False)
 
-#Using Response class provided by Django Rest Framework
-@api_view(['GET'])
-def list_posts(request):
-   if request.method == 'GET':
-      posts = Post.objects.all()  # get the querySet
-      serializer = PostSerializer(posts, many=True)
-      return Response(serializer.data, status=status.HTTP_200_OK)
+# Using Response class provided by Django Rest Framework
+# @api_view(['GET'])
+# def list_posts(request):
+#    if request.method == 'GET':
+#       posts = Post.objects.all()  # get the querySet
+#       serializer = PostSerializer(posts, many=True)
+#       return Response(serializer.data, status=status.HTTP_200_OK)
 
-@api_view(['GET'])   
-def post_detail(request, pk):
-  post = Post.objects.get(id=pk)  # get the specific post
-  serializer = PostSerializer(post, many=False)
-  #return JsonResponse(serializer.data)
-  return Response(serializer.data, status=status.HTTP_200_OK)
+# @api_view(['GET'])
+# def post_detail(request, pk):
+#   post = Post.objects.get(id=pk)  # get the specific post
+#   serializer = PostSerializer(post, many=False)
+#   #return JsonResponse(serializer.data)
+#   return Response(serializer.data, status=status.HTTP_200_OK)
 
-@api_view(['POST'])
-def post_create(request):
-  serializer = PostSerializer(data=request.data)
-  if serializer.is_valid():
-    serializer.save()
-  return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-@api_view(['POST'])
-def post_update(request, pk):
-  post = Post.objects.get(id=pk)  # get the specific post
-  serializer = PostSerializer(instance=post, data=request.data)
-  if serializer.is_valid():
-    serializer.save()
-  return Response(serializer.data, status=status.HTTP_200_OK)
-
-@api_view(['DELETE'])
-def post_delete(request, pk):
-  post = Post.objects.get(id=pk)  # get the specific post
-  post.delete()
-  return Response('Delete successful', status=status.HTTP_200_OK)
+# @api_view(['POST'])
+# def post_create(request):
+#   serializer = PostSerializer(data=request.data)
+#   if serializer.is_valid():
+#     serializer.save()
+#   return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
+# @api_view(["POST"])
+# def post_update(request, pk):
+#     post = Post.objects.get(id=pk)  # get the specific post
+#     serializer = PostSerializer(instance=post, data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-
-
+# @api_view(["DELETE"])
+# def post_delete(request, pk):
+#     post = Post.objects.get(id=pk)  # get the specific post
+#     post.delete()
+#     return Response("Delete successful", status=status.HTTP_200_OK)
